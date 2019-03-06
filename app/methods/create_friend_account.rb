c1d3c2b0c81c5f966adae2db53bd25b55f017991
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-03-06 17:06:26 -0600

    added create_friend_account method

---
+++ app/methods/create_friend_account.rb
@@ -0,0 +1,21 @@
+def create_friend_account
+    puts "\e[H\e[2J"
+    puts "Welcome to GenLINK!"
+   
+    name = $prompt.ask("What is your name?")
+   
+    email = $prompt.ask('What is your email?') do |q|
+      q.validate(/\A\w+@\w+\.\w+\Z/)
+        q.messages[:valid?] = 'Invalid email address'
+   
+    password = $prompt.mask('Password?', mask: ($prompt.decorate('❤ ', :magenta)))
+      #password.validate(/[a-z\ ]{5,15}/)
+   
+    dob = $prompt.ask('Please put in your DOB (yyyy/mm/dd)', convert: :date )
+      now = Time.now.utc.to_date
+      age = now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  
+    Friend.create(name: name, email: email, password: password, dob: dob)
+    end
+  
+   end
