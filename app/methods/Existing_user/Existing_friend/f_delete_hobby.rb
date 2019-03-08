4738956ae71fcbd968197ec10e979c3b4d2aa263
josieyao <yao.josephine@yahoo.com>
2019-03-08 12:13:39 -0600

    Revised delete hobby method for existing senior and existing friend so that the user can only delete a hobby that they have previously selected as their hobby.

--- app/methods/Existing_user/Existing_friend/f_delete_hobby.rb
+++ app/methods/Existing_user/Existing_friend/f_delete_hobby.rb
@@ -1,17 +1,29 @@
 def f_delete_hobby(current_friend)
     puts "\e[H\e[2J"
 
-   selected_hobby_to_delete = $prompt.select("Which hobby would you like to delete?") do |menu|
-        Hobby.all.each do |hobby|
+    friends = Friendhobby.where( friend_id: current_friend.id )
+
+    #friend_id, hobby_id
+
+    hobby_ids = friends.map {|friend| friend.hobby_id }
+
+    #hobby_id number
+
+    hobbies_for_a_friend = Hobby.where(id: hobby_ids)
+    #hobbies instances in an array
+
+    pick_hobby_to_delete = $prompt.select("Which hobby would you like to delete?") do |menu|
+        hobbies_for_a_friend.all.each do |hobby|
         menu.choice hobby.hobby
         end
     end
+    #1 hobby that they want to delete 
 
-    hobby_obj = Hobby.find_by(hobby: selected_hobby_to_delete)
+    hobby_obj = Hobby.where(hobby: pick_hobby_to_delete)
 
-    delete_this_hobby = Friendhobby.find_by(friend_id: current_friend.id, hobby_id: hobby_obj.id)
+    delete_this_hobby = Friendhobby.find_by(friend_id: current_friend.id, hobby_id: hobby_obj[0].id)
 
-    delete_new_hobby = Friendhobby.destroy(delete_this_hobby.id)
+    delete_now = Friendhobby.destroy(delete_this_hobby.id)
 
     press_any_key
     f_existing_user_login(current_friend)
--- app/methods/Existing_user/Existing_senior/s_delete_hobby.rb
+++ app/methods/Existing_user/Existing_senior/s_delete_hobby.rb
@@ -1,18 +1,30 @@
 def s_delete_hobby(current_senior)
     puts "\e[H\e[2J"
+    
+    seniors = Seniorhobby.where( senior_id: current_senior.id )
 
-   selected_hobby_to_delete = $prompt.select("Which hobby would you like to delete?") do |menu|
-        Hobby.all.each do |hobby|
+    #friend_id, hobby_id
+
+    hobby_ids = seniors.map {|senior| senior.hobby_id }
+
+    #hobby_id number
+
+    hobbies_for_a_senior= Hobby.where(id: hobby_ids)
+    #hobbies instances in an array
+
+    pick_hobby_to_delete = $prompt.select("Which hobby would you like to delete?") do |menu|
+        hobbies_for_a_senior.all.each do |hobby|
         menu.choice hobby.hobby
         end
     end
+    #1 hobby that they want to delete 
 
-    hobby_obj = Hobby.find_by(hobby: selected_hobby_to_delete)
+    hobby_obj = Hobby.where(hobby: pick_hobby_to_delete)
 
-    delete_this_hobby = Seniorhobby.find_by(senior_id: current_senior.id, hobby_id: hobby_obj.id)
+    delete_this_hobby = Seniorhobby.find_by(senior_id: current_senior.id, hobby_id: hobby_obj[0].id)
 
-    delete_new_hobby = Seniorhobby.destroy(delete_this_hobby.id)
+    delete_now = Seniorhobby.destroy(delete_this_hobby.id)
 
     press_any_key
-    s_existing_user_login(current_senior)
+    f_existing_user_login(current_senior)
 end
